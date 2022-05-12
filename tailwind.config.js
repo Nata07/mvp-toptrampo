37761543ae45c9e6517ede1a73dfbf91c10fe015
Natanael <nata.mw@hotmail.com>
2022-05-12 15:13:39 -0300

    changed form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  node:'jit', //cria somente o que precisa no projeto e nao cria o que é desnecessario
+  mode:'jit', //cria somente o que precisa no projeto e nao cria o que é desnecessario
   content: [
     './src/components/**/*.tsx',
     './src/pages/**/*.tsx',
